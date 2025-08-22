33a5665df9498fe70d6ddf1525633b00f1f3747e
Maicol Kutchoukov <maicol.j.95@gmail.com>
2025-08-22 16:16:40 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: { port: 5173 },
-  build: { outDir: 'dist' }
+  build: { outDir: 'dist' },
+  base: '/', 
 })
